5ee27523b48fde72f44534c35101f72426e43ff4
markedwards1 <103796441+markedwards1@users.noreply.github.com>
2025-12-11 17:28:14 +0800

    manifest and sw

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'pwd-logger-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png'
+  // add './icon-512.png' too if you have it
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
